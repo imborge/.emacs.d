1508b0853f49c51f83750bf46cc67dba135769ae
Børge André Jensen <borgeajensen@gmail.com>
2018-07-20 13:52:57 +0200

    fix

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -1,9 +1,10 @@
 (defun add-language-pragma ()
   (interactive)
+  (evil-insert 1) ;; Put in insert mode
   (goto-char 0) ;; Go to start of file
   (insert "{-# LANGUAGE  #-}\n") ;; Insert text
-  (goto-char 14) ;; Put cursor in place where language extensions should be
-  (evil-insert 1)) ;; Put in insert mode
+  (goto-char 14)) ;; Put cursor in place where language extensions should be
+
 
 (add-hook 'haskell-mode-hook
   (lambda ()
