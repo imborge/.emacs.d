262010dcffa97fe61637ceacd98f1cee772e1793
Børge André Jensen <borgeajensen@gmail.com>
2018-09-10 13:14:58 +0200

    added funcs for haskell language pragmas

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -5,7 +5,19 @@
   (insert "{-# LANGUAGE  #-}\n") ;; Insert text
   (goto-char 14)) ;; Put cursor in place where language extensions should be
 
+(defun add-overloaded-strings ()
+  (interactive)
+  (let ((p (point))
+        (s "{-# LANGUAGE OverloadedStrings #-}\n"))
+    (evil-insert 1)
+    (goto-char 0)
+    (insert s)
+    (goto-char (+ p (length s)))))
 
 (add-hook 'haskell-mode-hook
   (lambda ()
-    (local-set-key (kbd "C-x C-å") 'add-language-pragma)))
+    (local-set-key (kbd "C-x C-å +") 'add-language-pragma)))
+
+(add-hook 'haskell-mode-hook
+  (lambda ()
+    (local-set-key (kbd "C-x C-å s"))))
