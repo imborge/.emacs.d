043f700ca054fdb60f9d8bfb45177201d892af67
Børge André Jensen <borgeajensen@gmail.com>
2018-09-21 07:22:51 +0200

    Removed evil stuff from haskell language pragma functions

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -1,6 +1,5 @@
 (defun add-language-pragma ()
   (interactive)
-  (evil-insert 1) ;; Put in insert mode
   (goto-char 0) ;; Go to start of file
   (insert "{-# LANGUAGE  #-}\n") ;; Insert text
   (goto-char 14)) ;; Put cursor in place where language extensions should be
@@ -9,7 +8,6 @@
   (interactive)
   (let ((p (point))
         (s "{-# LANGUAGE OverloadedStrings #-}\n"))
-    (evil-insert 1)
     (goto-char 0)
     (insert s)
     (goto-char (+ p (length s)))))
