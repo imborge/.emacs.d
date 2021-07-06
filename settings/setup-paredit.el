f047040417fa3b93664795fd51f35d92da48fa16
Børge André Jensen <borgeajensen@gmail.com>
2021-07-07 00:08:17 +0200

    Changed paredit keybindings from Meta

--- settings/setup-paredit.el
+++ settings/setup-paredit.el
@@ -6,22 +6,22 @@
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode 1)))
 
 ;; Change nasty paredit keybindings
-(defvar my-nasty-paredit-keybindings-remappings
-  '(("M-s"         "s-s"         paredit-splice-sexp)
-    ("M-<up>"      "s-<up>"      paredit-splice-sexp-killing-backward)
-    ("M-<down>"    "s-<down>"    paredit-splice-sexp-killing-forward)
-    ("C-<right>"   "s-<right>"   paredit-forward-slurp-sexp)
-    ("C-<left>"    "s-<left>"    paredit-forward-barf-sexp)
-    ("C-M-<left>"  "s-S-<left>"  paredit-backward-slurp-sexp)
-    ("C-M-<right>" "s-S-<right>" paredit-backward-barf-sexp)))
+;; (defvar my-nasty-paredit-keybindings-remappings
+;;   '(("M-s"         "s-s"         paredit-splice-sexp)
+;;     ("M-<up>"      "s-<up>"      paredit-splice-sexp-killing-backward)
+;;     ("M-<down>"    "s-<down>"    paredit-splice-sexp-killing-forward)
+;;     ("C-<right>"   "s-<right>"   paredit-forward-slurp-sexp)
+;;     ("C-<left>"    "s-<left>"    paredit-forward-barf-sexp)
+;;     ("C-M-<left>"  "s-S-<left>"  paredit-backward-slurp-sexp)
+;;     ("C-M-<right>" "s-S-<right>" paredit-backward-barf-sexp)))
 
 (define-key paredit-mode-map (kbd "s-r") 'paredit-raise-sexp)
 
-(--each my-nasty-paredit-keybindings-remappings
-  (let ((original (car it))
-        (replacement (cadr it))
-        (command (car (last it))))
-    (define-key paredit-mode-map (read-kbd-macro original) nil)
-    (define-key paredit-mode-map (read-kbd-macro replacement) command)))
+;; (--each my-nasty-paredit-keybindings-remappings
+;;   (let ((original (car it))
+;;         (replacement (cadr it))
+;;         (command (car (last it))))
+;;     (define-key paredit-mode-map (read-kbd-macro original) nil)
+;;     (define-key paredit-mode-map (read-kbd-macro replacement) command)))
 
 (provide 'setup-paredit)
